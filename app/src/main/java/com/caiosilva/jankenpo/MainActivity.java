8ecf21924680ec9105e31041b09f80f2f55e3723
Caio <caio.silva@ab-inbev.com>
2024-02-26 10:33:02 -0300

    chore: create jankenpo project using viewbinding

--- app/src/main/java/com/caiosilva/jankenpo/MainActivity.java
+++ app/src/main/java/com/caiosilva/jankenpo/MainActivity.java
@@ -53,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
 
         switch (escolhaApp) {
             case "Pedra":
-
                 break;
             case "Papel":
                 break;
